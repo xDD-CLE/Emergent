fe568dba8dccf68fa9c1dad928fc21f93732ec17
Dave Shah <dave.shah@genomoncology.com>
2015-10-21 06:50:41 -0400

    Initial tinkering with dsl

---
+++ src/main/java/com/xdd/Spec.java
@@ -0,0 +1,13 @@
+package com.xdd;
+
+import java.util.function.Function;
+
+public class Spec {
+
+    public static void it(String description, Function<Void,Void> test) {
+
+    }
+
+
+
+}
---
+++ src/test/java/com/xdd/SpecifierTest.java
@@ -0,0 +1,16 @@
+package com.xdd;
+
+
+import static com.xdd.Spec.it;
+import static org.junit.Assert.*;
+
+public class SpecifierTest { {
+
+    it("Allows me to describe my tests simply", $-> {
+
+        assertTrue("This should work",true);
+
+
+        return null;
+    });
+} }
