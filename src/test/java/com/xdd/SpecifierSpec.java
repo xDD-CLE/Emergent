a9588877cbf364aef00be6e30f43cb31fce3aa08
Dave Shah <dave.shah@genomoncology.com>
2015-10-21 07:04:53 -0400

    WIP - still fleshing out idears

--- src/test/java/com/xdd/SpecifierSpec.java
+++ src/test/java/com/xdd/SpecifierSpec.java
@@ -4,10 +4,13 @@ package com.xdd;
 import static com.xdd.Spec.it;
 import static org.junit.Assert.*;
 
-public class SpecifierTest { {
+public class SpecifierSpec { {
 
+    //TODO: I wonder if passing in (this) could give me a hook to run within the spec. But is there a cleaner way?
     it("Allows me to describe my tests simply", () -> {
 
         assertTrue("This should work",true);
     });
+
+
 } }
